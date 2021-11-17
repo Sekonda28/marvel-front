466b2cf2f8e30201fab66c91988682d87c9d39cb
Matt Caswell <matt.caswell@hotmail.com>
2021-11-17 15:02:25 +0100

    personnage page basic request

--- src/pages/Personnages.js
+++ src/pages/Personnages.js
@@ -1,5 +1,45 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Personnages = () => {
-  return <div>Home</div>;
-}
+  const [charData, setCharData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://marvel-api-back.herokuapp.com/characters"
+        );
+        setCharData(response.data);
+        setIsLoading(false);
+        console.log(charData);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    fetchData();
+  }, [charData]);
+  return (
+    <div className="Personnages">
+      {isLoading ? (
+        <div>Page loading ...</div>
+      ) : (
+        <div>
+          {" "}
+          {charData.map((char, index) => {
+            return (
+              <div className="char-card">
+                <p>ff</p>
+                <p>fdfdf</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Personnages;
