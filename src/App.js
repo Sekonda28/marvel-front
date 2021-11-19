aa7f79cf768d905e37f0601ab64e3aa19db47a6a
Matt Caswell <matt.caswell@hotmail.com>
2021-11-19 09:34:17 +0100

    first changes to Favourties

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Personnages from "./pages/Personnages";
--- src/pages/Favoris.js
+++ src/pages/Favoris.js
@@ -1,23 +1,43 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Favoris = ({ setFavorisList, favorisList }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [comicData, setComicData] = useState();
   useEffect(() => {
-    try {
-      const favourites = localStorage.getItem("favourites");
-      const newTab = [];
-      if (favourites !== null) {
-        newTab.push(favourites.split(","));
-        setFavorisList(newTab);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://marvel-api-back.herokuapp.com/comics"
+        );
+        setComicData(response.data);
+        const favourites = localStorage.getItem("favourites");
+        console.log(favourites);
+        const newTab = [];
+        if (favourites !== null) {
+          newTab.push(favourites.split(","));
+          setFavorisList(newTab);
+          console.log(favorisList);
+        }
+
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
       }
+    };
 
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, [favorisList, setFavorisList]);
+    fetchData();
+  }, [setFavorisList, favorisList]);
 
-  return <div> {isLoading ? <p>Loading...</p> : { favorisList }}</div>;
+  return (
+    <div className="Favoris">
+      
+      {isLoading ? (
+        <div className="isLoading">Page Loading...</div>
+      ) : (
+        <div>{favorisList}</div>
+      )}
+    </div>
+  );
 };
-
 export default Favoris;
